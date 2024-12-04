a02e715cbcf734b6e3a8d00a2d353f0dbc6637a1
Néodel <loic.kerboriou@gmail.com>
2024-12-04 01:39:18 +0100

    AOC: Update day two to take aoc_lube lib as import

--- 2024/day_02.py
+++ 2024/day_02.py
@@ -1,31 +1,14 @@
-######################
-####### PART 1 #######
-######################
+import aoc_lube
 
-filename = "data"
+RAW = aoc_lube.fetch(year=2024, day=2)
 
-safe_count = 0
-reports = []
+REPORTS = []
 
 def is_safe(report):
     report = [int(x) for x in report]
     is_increasing = all(report[level] < report[level + 1] <= report[level] + 3 for level in range(len(report) - 1))
     is_decreasing = all(report[level] > report[level + 1] >= report[level] - 3 for level in range(len(report) - 1))
     return is_increasing or is_decreasing
-
-with open(filename, "r") as f:
-    for line in f:
-        reports.append(line.strip().split())
-
-for report in reports:
-        if is_safe(report):
-            safe_count += 1
-
-print("Le résultat est : " + str(safe_count) + ".")
-
-######################
-####### PART 2 #######
-######################
 
 def is_almost_safe(report):
     if not is_safe(report):
@@ -36,9 +19,23 @@ def is_almost_safe(report):
         return False
     return True
 
-safe_count = 0
-for report in reports:
-    if is_almost_safe(report):
-        safe_count += 1
+def part_one():
+    safe_count = 0
+    for line in RAW.split("\n"):
+        REPORTS.append(line.split())
 
-print("Le nouveau résultat est : " + str(safe_count) + ".")
+    for report in REPORTS:
+        if is_safe(report):
+            safe_count += 1
+
+    return safe_count
+
+def part_two():
+    safe_count = 0
+    for report in REPORTS:
+        if is_almost_safe(report):
+            safe_count += 1
+    return safe_count
+
+aoc_lube.submit(year=2024, day=2, part=1, solution=part_one)
+aoc_lube.submit(year=2024, day=2, part=2, solution=part_two)
